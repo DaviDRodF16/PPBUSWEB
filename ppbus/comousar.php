b45d9f941ffc766a44ce73b9b273eb7f29aabfee
DaviDRodF16 <106616434+DaviDRodF16@users.noreply.github.com>
2022-06-03 14:40:44 -0300

    Add files via upload

---
+++ ppbus/comousar.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="app.css">
+    <title> Como Usar? </title>
+</head>
+<body>
+    <h1> PPBUS SUPORTE </h1>
+        <p> Olá, estamos aqui para esclarecer-lhe dúvidas acerca do funcionamento de nosso site. <p>
+
+        <p> Caso você queira saber as linhas de ônibus que passam pela Leste-Oeste, vá ao menu inicial e clique em <a href="linhasonibus.php"> Linhas de ônibus </a> </p>
+        <p> Caso você queira utilizar </p>
+</body>
+</html>
---
+++ ppbus/index.php
@@ -0,0 +1,27 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="css/app.css">   []
+     <title>PPBUS</title>
+ </head>
+ <body>
+     <h1> PPBUS </h1>
+    <ul>
+        <li> <a href="index.php"> Ínicio</a></li>
+        <li> <a href="comousar.php"> Ajuda </a> </li>
+        <li> <a href="linhasonibus.php"> Linhas de Ônibus </a> </li>
+
+    </ul>
+    <p> Bem vindo ao PPBUS, aplicativo feito para te ajudar em sua locomoção pela Barra do Ceará. Nós entendemos o quanto o transporte público é essencial para muitas pessoas, por este motivo, criamos o PPBUS.
+O PPBUS mostra todos os ônibus, horários e suas respectivas paradas que paradas que passam pela Leste-Oeste.</p> 
+    <p> Assim, nós oferecemos aos nossos usuários independência e facilidade ao se deslocar pela Barra do Ceará, ajudando principalmente aos alunos da querida escola: Paulo Petrola. </p>
+
+    
+    <script src="js/jquery360.min.js"> </script>
+    <script src="js/app.js"> <script>
+</body>
+ </html>
