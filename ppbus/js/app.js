f0901cebc97878118da3f42710ce74735b7c17ef
DaviDRodF16 <106616434+DaviDRodF16@users.noreply.github.com>
2022-06-09 10:54:59 -0300

    começando js
    tentando fazer alerts e receber as informações

---
+++ ppbus/js/app.js
@@ -0,0 +1,5 @@
+let nome = document.querySelector("#nome");
+let email = document.querySelector("#email");
+let simnao = document.querySelector('input[name=simnao]:checked').value;
+let opiniao = document.querySelector("#opiniao").value;
+console.log(opiniao);
