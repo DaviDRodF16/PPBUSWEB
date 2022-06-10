8b9f525acf8dd68ffadb8e8269458e1ab7bca153
Ednalperer4 <103148502+Ednalperer4@users.noreply.github.com>
2022-06-10 14:30:57 -0300

    complemento do css do rafael
    coloquei o css de alguns elementos do inicio

--- ppbus/index.php
+++ ppbus/index.php
@@ -9,21 +9,30 @@
      <title>PPBUS</title>
  </head>
  <body>
+     <div>
       <header>
          <nav> 
          <h1> PPBUS </h1>       
         <ul class="lista-home">
-        <li> <a href="index.php"> Ínicio</a></li>
-        <li> <a href="comousar.php"> Ajuda </a> </li>
-        <li> <a href="onibus.php"> Ônibus </a> </li>
+        <li> <a href="index.php" class="inicio"> Ínicio</a></li>
+        <li> <a href="comousar.php" class="comusar"> Ajuda </a> </li>
+        <li> <a href="onibus.php" class="onibus"> Ônibus </a> </li>
      </ul>
    </nav>
 </header>
-    <p> Bem vindo ao PPBUS, aplicativo feito para te ajudar em sua locomoção pela Barra do Ceará. Nós entendemos o quanto o transporte público é essencial para muitas pessoas, por este motivo, criamos o PPBUS.
+</div>
+    
+    <div class="container">
+        <p> Bem vindo ao PPBUS, aplicativo feito para te ajudar em sua locomoção pela Barra do Ceará. Nós entendemos o quanto o transporte público é essencial para muitas pessoas, por este motivo, criamos o PPBUS.
 O PPBUS mostra todos os ônibus, horários e suas respectivas paradas que paradas que passam pela Leste-Oeste.</p> 
     <p> Assim, nós oferecemos aos nossos usuários independência e facilidade ao se deslocar pela Barra do Ceará, ajudando principalmente aos alunos da querida escola: Paulo Petrola. </p>
+  <div class="opniao">
+    <h3> <a href="coments.php" id="opiniao"> Nos dê sua opinião! </a> </h3> 
+    </div>
+    <img src="iron-maiden-666-onibus-fortaleza.jpg" class="imagem">
 
-    <h3> <a href="coments.php" id="opiniao"> Nos dê sua opinião! </a> </h3>
+</div>
+    
     <script src="js/jquery360.min.js"> </script>
     <script src="js/app.js"> <script>
 </body>
