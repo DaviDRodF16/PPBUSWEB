b17d0f87b36d5b5eccf3e6fe5f19a4add8afaa42
Rafa72 <106617724+Rafa72@users.noreply.github.com>
2022-06-15 16:01:34 -0300

    Delete ppbus/js directory

--- ppbus/js/app.js
+++
@@ -1,5 +0,0 @@
-let nome = document.querySelector("#nome");
-let email = document.querySelector("#email");
-let simnao = document.querySelector('input[name=simnao]:checked').value;
-let opiniao = document.querySelector("#opiniao").value;
-console.log(opiniao);
